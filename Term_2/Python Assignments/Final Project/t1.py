873fca72789c9843c5f25f32b18c118715cb1644
forstuffapps <forstuffapps@gmail.com>
2023-04-19 23:07:40 -0400

    python final

--- Term_2/Python Assignments/Final Project/t1.py
+++ Term_2/Python Assignments/Final Project/t1.py
@@ -1,9 +1,21 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-x = np.linspace(0, 10, 100)
 
-plt.plot(x, np.sin(x))
-plt.plot(x, np.cos(x))
+# Avinash Reddy Baddam , C0877290
 
-plt.show()
+plt.title("Avinash project")
+
+
+x = np.random.randint(100, size=10)
+
+#ax = plt.axes()
+
+ax = plt.gca()
+ax.set_facecolor("#221E22")
+
+plt.plot(x, marker='o', color='#ECA72C')
+plt.savefig('chart.png')
+plt.show() 
+
+
